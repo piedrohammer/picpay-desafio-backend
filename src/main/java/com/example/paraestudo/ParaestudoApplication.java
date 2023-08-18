9d2b9f7348cfa7bafc4dc70c50c50aef8e0cc9f1
piedrohammer <piiedrohammer@gmail.com>
2023-08-18 12:07:53 -0300

    first commit

---
+++ src/main/java/com/example/paraestudo/ParaestudoApplication.java
@@ -0,0 +1,13 @@
+package com.example.paraestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaestudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaestudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paraestudo/ParaestudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paraestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaestudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
