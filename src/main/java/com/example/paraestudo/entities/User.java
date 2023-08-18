1675eae540c09cca87b2a37513f01d16431a2e99
piedrohammer <piiedrohammer@gmail.com>
2023-08-18 12:09:02 -0300

    create user userType

---
+++ src/main/java/com/example/paraestudo/entities/User.java
@@ -0,0 +1,40 @@
+package com.example.paraestudo.entities;
+
+import com.example.paraestudo.enums.UserType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    public String password;
+
+    public BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
---
+++ src/main/java/com/example/paraestudo/enums/UserType.java
@@ -0,0 +1,6 @@
+package com.example.paraestudo.enums;
+
+public enum UserType {
+    COMMOM,
+    MERCHANT
+}
